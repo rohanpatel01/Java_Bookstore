55d0a039133c6353675eb92ffcac6120d542e806
rohanpatel01 <rohanppatel01@gmail.com>
2024-04-20 10:29:19 -0500

    saving before trying two different project with client side inventory

--- src/Shared/Book.java
+++ src/Shared/Book.java
@@ -11,10 +11,14 @@ public class Book extends LibraryItem implements Serializable {
          super(title, summaryDescription);
 
          // only increment the library ID if one with same title does not already exist
-         if (!Inventory.bookList.containsKey(title)) {
-             this.itemID = LibraryItem.LIBRARY_ID;
-             LibraryItem.LIBRARY_ID += 1;
-         }
+//         if (!Inventory.bookList.containsKey(title)) {
+//             this.itemID = LibraryItem.LIBRARY_ID;
+//             LibraryItem.LIBRARY_ID += 1;
+//         }
+
+         // make this only happen for books that are not already in list like ^^
+         this.itemID = LibraryItem.LIBRARY_ID;
+         LibraryItem.LIBRARY_ID += 1;
 
          this.itemType = "BOOK";
          this.author = author;
